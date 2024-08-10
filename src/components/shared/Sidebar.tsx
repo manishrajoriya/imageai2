eae9a275c8e6594573e139acf16682dffc537c7e
manish <gkrajoriya3@gmail.com>
2024-08-10 15:57:02 +0530

    h

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -7,10 +7,47 @@ import { usePathname } from 'next/navigation'
 import { SignedOut, SignIn, SignInButton } from '@clerk/nextjs'
 import { SignedIn, UserButton } from '@clerk/clerk-react'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 function SideBar() {
     const pathname = usePathname();
   return (
-    <aside className='sidebar'>
+    
+<>
+        <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+   
+    <DropdownMenuSeparator />
+    {
+         navLinks.map((link)=> {
+             const isActive = link.route === pathname;
+             return(
+                 <DropdownMenuItem>
+             <li key={link.label} className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white " : "text-gray-700"}`}>
+                 <Link href={link.route}>
+                 <Image src={link.icon} alt='icon' width={24} height={24} className={` ${isActive && 'brightness-200'}`}/>
+                 {link.label}
+                 </Link>
+                 
+             </li>
+             </DropdownMenuItem>
+         )})
+            
+ }
+   
+  </DropdownMenuContent>
+</DropdownMenu>
+
+<aside className='sidebar shadow-sm'>
         <div className='flex size-full flex-col gap-3'>
             <Link href="/"  className='sidebar-logo'>
             <Image src="/logo.png" alt='logo' width={180} height={280} />
@@ -47,6 +84,7 @@ function SideBar() {
         </div>
 
     </aside>
+    </>
   )
 }
 
